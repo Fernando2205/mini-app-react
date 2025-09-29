5919908f9a7f6f76804f7b0104c09e16ea51ce96
Fernando Palacios <deliopalaciosviafara@gmail.com>
2025-09-29 16:07:22 -0500

    update main setup and styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { FiltersProvider } from './context/filters.jsx'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <FiltersProvider>
-    <App />
-  </FiltersProvider>
-
+  <BrowserRouter>
+    <FiltersProvider>
+      <App />
+    </FiltersProvider>
+  </BrowserRouter>
 )
